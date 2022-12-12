0f568686c5d5d3544cfe59e1ad3c231d255243a8
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2022-12-11 20:56:50 -0600

    search done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider, keyframes } from "styled-components"
 
 const result = [
   {
@@ -45,7 +45,13 @@ function App() {
     <ThemeProvider theme={theme}>
       <Wrapper>
         <Header>
-          <SearchInput placeholder="Busca una raza" />
+          <SearchWrapper
+            isLoading={true}
+            onClose={() => alert("cierrate sesamo")}
+          >
+            <SearchInput placeholder="Busca una raza" />
+            <Close onClick={() => alert("cierrate sesamo")} />
+          </SearchWrapper>
         </Header>
         <Results>
           {result.map((b) => (
@@ -63,14 +69,61 @@ function App() {
 
 export default App
 
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(-360deg);
+  }
+`
 const SearchInput = styled.input`
+  background-color: #33333300;
+  border: none;
+  padding: 20px 0px;
+  width: 100%;
+  text-indent: 40px;
+`
+
+const SearchWrapper = styled.div`
   border: 1px solid ${({ theme }) => theme.border};
   border-radius: 5px;
   margin: 20px 10%;
   width: 70%;
-  padding: 10px;
-  background: ${({ theme }) => theme.bg};
+  padding: 0px 0px;
+  background-color: ${({ theme }) => theme.bg};
+  background: url(/assets/search.png) 10px center no-repeat;
+  position: relative;
+  overflow: hidden;
+  &::after {
+    opacity: ${({ isLoading }) => (isLoading ? 1 : 0)};
+    display: "block";
+    position: absolute;
+    height: 24px;
+    width: 24px;
+    top: 25%;
+    right: 50px;
+    content: "";
+    background: url(/assets/loading.png);
+    animation: ${rotate} 3s linear infinite;
+    z-index: 1000;
+    transition: opacity 0.2s;
+  }
 `
+const Close = styled.span`
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  top: 25%;
+  right: ${({ hasText }) => (hasText ? "10px" : "-20px")};
+  transition: all 0.2s;
+  content: "";
+  background: url(/assets/close.png) 0px 0px no-repeat;
+  background-size: cover;
+  cursor: pointer;
+`
+//Loader
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -84,7 +137,7 @@ const Wrapper = styled.div`
 `
 
 const Header = styled.div`
-  width: 100%;
+  width: 70%;
   text-align: center;
   padding: 10px;
 `
